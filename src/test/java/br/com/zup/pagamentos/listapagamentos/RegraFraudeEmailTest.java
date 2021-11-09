7930ecff2ee40fe2f2dd3fca3983370255c7a8c4
Jackson <jackson.mota@zup.com.br>
2021-11-09 13:09:39 -0300

    Refatoração de testes

--- src/test/java/br/com/zup/pagamentos/listapagamentos/RegraFraudeEmailTest.java
+++ src/test/java/br/com/zup/pagamentos/listapagamentos/RegraFraudeEmailTest.java
@@ -1,6 +1,5 @@
 package br.com.zup.pagamentos.listapagamentos;
 
-import br.com.zup.pagamentos.formapagamento.FormaPagamento;
 import br.com.zup.pagamentos.usuario.Usuario;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,34 +13,38 @@ class RegraFraudeEmailTest {
     private final RegraFraudeEmail regraFraudeEmail = new RegraFraudeEmail();
 
     @Test
-    @DisplayName("Deve retornar TRUE quando a forma é OFFLINE e email não começa com J")
+    @DisplayName("Deve aceitar quando a forma é OFFLINE e email não começa com J")
     void teste1() {
-        var usuario = new Usuario("email@a.com", DINHEIRO, CHEQUE, MAQUINA);
+        var usuario = new Usuario("email@a.com", DINHEIRO);
 
-        usuario.getFormasPagamento().forEach(forma -> assertTrue(regraFraudeEmail.aceita(usuario, forma)));
+        assertTrue(regraFraudeEmail.aceita(usuario, DINHEIRO));
+        assertTrue(regraFraudeEmail.aceita(usuario, CHEQUE));
+        assertTrue(regraFraudeEmail.aceita(usuario, MAQUINA));
     }
 
     @Test
-    @DisplayName("Deve retornar TRUE quando a forma é OFFLINE e email começa com J")
+    @DisplayName("Deve aceitar quando a forma é OFFLINE e email começa com J")
     void teste2() {
-        var usuario = new Usuario("jemail@a.com", DINHEIRO, CHEQUE, MAQUINA);
+        var usuario = new Usuario("jemail@a.com", DINHEIRO);
 
-        usuario.getFormasPagamento().forEach(forma -> assertTrue(regraFraudeEmail.aceita(usuario, forma)));
+        assertTrue(regraFraudeEmail.aceita(usuario, DINHEIRO));
+        assertTrue(regraFraudeEmail.aceita(usuario, CHEQUE));
+        assertTrue(regraFraudeEmail.aceita(usuario, MAQUINA));
     }
 
     @Test
-    @DisplayName("Deve retornar TRUE quando a forma é ONLINE e email não começa com J")
+    @DisplayName("Deve aceitar quando a forma é ONLINE e email não começa com J")
     void teste3() {
         var usuario = new Usuario("email@a.com", CARTAO_CREDITO);
 
-        usuario.getFormasPagamento().forEach(forma -> assertTrue(regraFraudeEmail.aceita(usuario, forma)));
+        assertTrue(regraFraudeEmail.aceita(usuario, CARTAO_CREDITO));
     }
 
     @Test
-    @DisplayName("Deve retornar FALSE quando a forma é ONLINE e email  começa com J")
+    @DisplayName("Não deve aceitar quando a forma é ONLINE e email  começa com J")
     void teste4() {
         var usuario = new Usuario("jemail@a.com", CARTAO_CREDITO);
 
-        usuario.getFormasPagamento().forEach(forma -> assertFalse(regraFraudeEmail.aceita(usuario, forma)));
+        assertFalse(regraFraudeEmail.aceita(usuario, CARTAO_CREDITO));
     }
 }
