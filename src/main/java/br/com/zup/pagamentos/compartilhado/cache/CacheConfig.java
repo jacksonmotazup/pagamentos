75b45acacdf7ad412f6f701f169d9ebd4c1bef34
Jackson <jackson.mota@zup.com.br>
2021-11-18 10:06:01 -0300

    adição de configuração de cache

---
+++ src/main/java/br/com/zup/pagamentos/compartilhado/cache/CacheConfig.java
@@ -0,0 +1,27 @@
+package br.com.zup.pagamentos.compartilhado.cache;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public Caffeine caffeineConfig() {
+        return Caffeine.newBuilder().expireAfterWrite(60, TimeUnit.MINUTES);
+    }
+
+    @Bean
+    public CacheManager cacheManager(Caffeine caffeine) {
+        var caffeineCacheManager = new CaffeineCacheManager();
+        caffeineCacheManager.setCaffeine(caffeine);
+        return caffeineCacheManager;
+    }
+}
