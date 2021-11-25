1d71607148b26047f0cbd508f966b5bec42be007
Jackson <jackson.mota@zup.com.br>
2021-11-25 17:23:45 -0300

    mudança entity

--- src/main/java/br/com/zup/pagamentos/listapagamentos/ListaPagamentosController.java
+++ src/main/java/br/com/zup/pagamentos/listapagamentos/ListaPagamentosController.java
@@ -3,9 +3,7 @@ package br.com.zup.pagamentos.listapagamentos;
 import br.com.zup.pagamentos.restaurante.RestauranteRepository;
 import br.com.zup.pagamentos.usuario.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -32,8 +30,7 @@ public class ListaPagamentosController {
     }
 
     @GetMapping
-    @Cacheable(value = "pagamentos")
-    public PagamentosResponse listaPagamentosUsuarioRestaurante(@Valid @RequestBody ListaPagamentosRequest request) {
+    public PagamentosResponse listaPagamentosUsuarioRestaurante(@Valid ListaPagamentosRequest request) {
         var restaurante = restauranteRepository.findById(request.restauranteId())
                 .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "Restaurante não encontrado"));
 
