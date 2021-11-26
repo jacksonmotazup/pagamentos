3308383e4a6d9a85fcaf8c9ba559235c5603400e
Jackson <jackson.mota@zup.com.br>
2021-11-26 09:36:33 -0300

    docker-compose

--- src/main/resources/db/migration/V2021.11.26.09.21.42__Criar_Indices_para_Formas_Pagamentos_de_Usuarios_e_Restaurantes.sql
+++
@@ -1,7 +0,0 @@
--- -- cria index para USUARIO_FORMAS_PAGAMENTO.usuario_id
--- CREATE INDEX usuario_formas_pag_usuario_id_idx
---           ON public.usuario_formas_pagamento USING btree (usuario_id);
---
--- -- cria index para RESTAURANTE_FORMAS_PAGAMENTO_ACEITAS.restaurante_id
--- CREATE INDEX restaurante_formas_pag_restaurante_id_idx
---           ON public.restaurante_formas_pagamento_aceitas USING btree (restaurante_id);
