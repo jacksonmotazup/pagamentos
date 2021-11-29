1dedd155ff712a31e14045c8658b292e1b86e188
Jackson <jackson.mota@zup.com.br>
2021-11-29 17:25:39 -0300

    Melhorias para performance

--- src/main/java/br/com/zup/pagamentos/restaurante/RestauranteRepository.java
+++ src/main/java/br/com/zup/pagamentos/restaurante/RestauranteRepository.java
@@ -1,6 +1,11 @@
 package br.com.zup.pagamentos.restaurante;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+    @Query("SELECT r FROM Restaurante r JOIN FETCH r.formasPagamentoAceitas WHERE r.id=?1")
+    Optional<Restaurante> findByIdComFormasPagamento(Long restauranteId);
 }
