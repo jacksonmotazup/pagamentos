9218f2ac861e6fb4e52be74efa10840f8a5e3c1c
Jackson <jackson.mota@zup.com.br>
2021-11-30 11:18:11 -0300

    commit

--- src/main/java/br/com/zup/pagamentos/listapagamentos/ListaPagamentosController.java
+++ src/main/java/br/com/zup/pagamentos/listapagamentos/ListaPagamentosController.java
@@ -3,6 +3,7 @@ package br.com.zup.pagamentos.listapagamentos;
 import br.com.zup.pagamentos.restaurante.RestauranteRepository;
 import br.com.zup.pagamentos.usuario.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +31,7 @@ public class ListaPagamentosController {
     }
 
     @GetMapping
+    @Cacheable("pagamentos")
     public PagamentosResponse listaPagamentosUsuarioRestaurante(@Valid ListaPagamentosRequest request) {
         var restaurante = restauranteRepository.findByIdComFormasPagamento(request.restauranteId())
                 .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "Restaurante não encontrado"));
